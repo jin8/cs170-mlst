f01c48f244bc19961674313a18ea70edc88cbe3f
Prateek <prateekredd@gmail.com>
2013-05-05 23:55:19 -0700

    fixed some bugs

--- src/mlstSolver.py
+++ src/mlstSolver.py
@@ -8,25 +8,41 @@ def solver(graph, output):
         d[v] = 0
       
     for v in graph.getVertices():
-        S2 = []
-        d2 =0
-        
+        S2 = {}
+        d2 = 0
         for u in graph.getAdjacentVertices(v):
             if not u in S[v] and len(set(S[u])&set(S2)) == 0:
                 d2 += 1
-                S2.extend(S[u])
+                S2[u] = S[u]
+                
         if d[v]+d2 >= 3:
-            for s in S2:
-                forest.append([s,v])
-                S[v] = list((set(S[v])|set(S[s])))
-                S[s] = S[v]
-                d[s] += 1
-                d[v] += 1
+            setv = S[v]
+            for u in S2.keys():
+                forest.append([u,v])
+                S[v] = list((set(S[v])|set(S[u])))
+                #S[u] = list((set(S[v])|set(S[u]))) #S[u] = S[v] | S[u]
+                d[u], d[v] = d[u] + 1, d[v] + 1
+            for n in S[v]:
+                S[n] = S[v]
+        print "v = " + str(v)
+        print "dict is" + str(S)
+        print "degree is" + str(d)
     
     for edge in forest:
-        output.addEdge(edge)
-		
-
+        output.addEdge(edge)    
     return output
 
         
+def breadthFirstSearch(graph):
+    vertices, visited, color = graph.getVertices(),[], []
+    currentColor = 0
+    while (len(vertices) != 0):
+        start = [vertices[0]]
+        while not len(start) != 0:
+            node = start.pop(0)    
+            if node not in visited:
+                visited.append(node)
+                start.extend(graph.getAdjacentVertices(node))
+                vertices.remove(node)
+                color[node] = currentColor
+        currentColor++
